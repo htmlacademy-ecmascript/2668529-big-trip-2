42ffb61182c804e92646fb8162874b88516ab8c1
chaykinks <chaykinks@gmail.com>
2025-12-16 20:08:45 +0500

    исправил форматы дат и продолжительности

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 
 const DATE_FORMAT = 'MMM DD';
 const TIME_FORMAT = 'HH:mm';
+const DATE_TIME_FORMAT = 'DD/MM/YY HH:mm';
 
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
@@ -15,11 +16,29 @@ function humanizePointTime(date) {
   return date ? dayjs(date).format(TIME_FORMAT) : '';
 }
 
+function humanizeDateTime(date) {
+  return date ? dayjs(date).format(DATE_TIME_FORMAT) : '';
+}
+
 function getEventDuration(dateFrom, dateTo) {
   const start = dayjs(dateFrom);
   const end = dayjs(dateTo);
   const diffMinutes = end.diff(start, 'minute');
-  return `${diffMinutes}M`;
+  const days = Math.floor(diffMinutes / (60 * 24));
+  const hours = Math.floor((diffMinutes % (60 * 24)) / 60);
+  const minutes = diffMinutes % 60;
+
+  const format = (value) => String(value).padStart(2, '0');
+  const durationParts = [];
+
+  if (days > 0) {
+    durationParts.push(`${format(days)}D`);
+  }
+  if (hours > 0 || days > 0) {
+    durationParts.push(`${format(hours)}H`);
+  }
+  durationParts.push(`${format(minutes)}M`);
+  return durationParts.join(' ');
 }
 
-export {getRandomArrayElement, humanizePointDate, humanizePointTime, getEventDuration};
+export {getRandomArrayElement, humanizePointDate, humanizePointTime, humanizeDateTime, getEventDuration};
--- src/view/new-form-view.js
+++ src/view/new-form-view.js
@@ -1,5 +1,5 @@
 import {createElement} from '../render.js';
-import {humanizePointDate} from '../utils';
+import {humanizeDateTime} from '../utils';
 import {POINTS_TYPE} from '../const';
 
 const createTypeTemplate = (type) => `<div class="event__type-item">
@@ -77,10 +77,10 @@ function createNewFormTemplate(point, offers, checkedOffers, destination) {
 
           <div class="event__field-group event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${humanizePointDate(dateFrom)}">
+            <input class="event__input event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${humanizeDateTime(dateFrom)}">
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
-            <input class="event__input event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${humanizePointDate(dateTo)}">
+            <input class="event__input event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${humanizeDateTime(dateTo)}">
           </div>
 
           <div class="event__field-group event__field-group--price">
