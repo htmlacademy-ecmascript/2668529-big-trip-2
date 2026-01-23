a843b0fed288a439d7b42de178efe7651bf6350b
chaykinks <chaykinks@gmail.com>
2026-01-23 15:50:58 +0500

    добавил модель для фильтра

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import TripPresenter from './presenter/trip-presenter.js';
 import PointsModel from './model/points-model.js';
 import FilterView from './view/filter-view.js';
+import FilterModel from './model/filter-model.js';
 import { generateFilterData } from './utils/filter.js';
 import { FilterType } from './const.js';
 import { render } from './framework/render.js';
@@ -8,6 +9,7 @@ import { render } from './framework/render.js';
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 const tripPresenter = new TripPresenter({tripEventsContainer, pointsModel});
 
 const filters = generateFilterData(pointsModel.points);
